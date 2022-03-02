c5ef934e1dc7f21bd0c65b26a4b685022be13308
김민재 <joshuara7235@gmail.com>
2022-03-02 15:39:35 +0900

    #2-5 Spring security : Logout filter API test code

--- src/main/java/io/security/basic_security/SecurityConfig.java
+++ src/main/java/io/security/basic_security/SecurityConfig.java
@@ -8,10 +8,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Configuration
@@ -25,12 +28,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .anyRequest().authenticated();
         http //인증 정책 설정
                 .formLogin()
-                //.loginPage("/loginPage") // login 하도록 하는 page로 이동 하는 url
+                .loginPage("/loginPage") // login 하도록 하는 page로 이동 하는 url
                 .defaultSuccessUrl("/")
                 .failureUrl("/login") // url 을 매핑해줌
                 .usernameParameter("userId") //default는 username이다
                 .passwordParameter("passwd") //default는 password이다.
-                .loginProcessingUrl("/login_proc") //form tag의 action url이 여기에 매핑되는 것임
+                .loginProcessingUrl("/login") //form tag의 action url이 여기에 매핑되는 것임. login 처리 api url 설정
                 .successHandler(new AuthenticationSuccessHandler() {
                     @Override
                     public void onAuthenticationSuccess(final HttpServletRequest request, final HttpServletResponse response, final Authentication authentication) throws IOException, ServletException {
@@ -45,8 +48,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         response.sendRedirect("/login");
                     }
                 })
-                .permitAll()
-        ;
+                .permitAll();
+        http
+                .logout()
+                .logoutUrl("/logout") // 원칙적으로 post 방식으로 logout 처리를 한다.
+                .logoutSuccessUrl("/login")
+                .addLogoutHandler(new LogoutHandler() {
+                    @Override
+                    public void logout(final HttpServletRequest request, final HttpServletResponse response, final Authentication authentication) {
+                        HttpSession session = request.getSession();
+                        session.invalidate(); //session을 무력화 시키는것
+                    }
+                })
+                //logoutSuccessUrl과 비슷하지만, handler를 구현하면 더 많은 로직을 안에 넣을 수 있다.
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(final HttpServletRequest request, final HttpServletResponse response, final Authentication authentication) throws IOException, ServletException {
+                        response.sendRedirect("/login");
+                    }
+                })
+                //쿠키의 이름을 적는다. 로그아웃시 이름을 적은 쿠키를 지워주는 것.
+                .deleteCookies("remember-me")
+                ;
     }
 
 }
