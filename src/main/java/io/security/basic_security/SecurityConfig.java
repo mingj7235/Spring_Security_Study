be7e2c070bac10f1fe5e672d0a2232b3f8b08f0c
김민재 <joshuara7235@gmail.com>
2022-03-04 18:37:42 +0900

    #2-6 Remember-me function

--- src/main/java/io/security/basic_security/SecurityConfig.java
+++ src/main/java/io/security/basic_security/SecurityConfig.java
@@ -1,11 +1,13 @@
 package io.security.basic_security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
@@ -19,36 +21,41 @@ import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity //web 보안을 활성하게 해주는 어노테이션
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http //인가 정책 설정
                 .authorizeRequests()
                     .anyRequest().authenticated();
+        //로그인 인증정책
         http //인증 정책 설정
-                .formLogin()
-                .loginPage("/loginPage") // login 하도록 하는 page로 이동 하는 url
-                .defaultSuccessUrl("/")
-                .failureUrl("/login") // url 을 매핑해줌
-                .usernameParameter("userId") //default는 username이다
-                .passwordParameter("passwd") //default는 password이다.
-                .loginProcessingUrl("/login") //form tag의 action url이 여기에 매핑되는 것임. login 처리 api url 설정
-                .successHandler(new AuthenticationSuccessHandler() {
-                    @Override
-                    public void onAuthenticationSuccess(final HttpServletRequest request, final HttpServletResponse response, final Authentication authentication) throws IOException, ServletException {
-                        System.out.println("authentication : " + authentication.getName());
-                        response.sendRedirect("/");
-                    }
-                })
-                .failureHandler(new AuthenticationFailureHandler() {
-                    @Override
-                    public void onAuthenticationFailure(final HttpServletRequest request, final HttpServletResponse response, final AuthenticationException exception) throws IOException, ServletException {
-                        System.out.println("exception : " + exception.getMessage());
-                        response.sendRedirect("/login");
-                    }
-                })
-                .permitAll();
+                .formLogin();
+//                .loginPage("/loginPage") // login 하도록 하는 page로 이동 하는 url
+//                .defaultSuccessUrl("/")
+//                .failureUrl("/login") // url 을 매핑해줌
+//                .usernameParameter("userId") //default는 username이다
+//                .passwordParameter("passwd") //default는 password이다.
+//                .loginProcessingUrl("/login") //form tag의 action url이 여기에 매핑되는 것임. login 처리 api url 설정
+//                .successHandler(new AuthenticationSuccessHandler() {
+//                    @Override
+//                    public void onAuthenticationSuccess(final HttpServletRequest request, final HttpServletResponse response, final Authentication authentication) throws IOException, ServletException {
+//                        System.out.println("authentication : " + authentication.getName());
+//                        response.sendRedirect("/");
+//                    }
+//                })
+//                .failureHandler(new AuthenticationFailureHandler() {
+//                    @Override
+//                    public void onAuthenticationFailure(final HttpServletRequest request, final HttpServletResponse response, final AuthenticationException exception) throws IOException, ServletException {
+//                        System.out.println("exception : " + exception.getMessage());
+//                        response.sendRedirect("/login");
+//                    }
+//                })
+//                .permitAll();
+        // 로그아웃 인증 정책
         http
                 .logout()
                 .logoutUrl("/logout") // 원칙적으로 post 방식으로 logout 처리를 한다.
@@ -68,8 +75,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     }
                 })
                 //쿠키의 이름을 적는다. 로그아웃시 이름을 적은 쿠키를 지워주는 것.
-                .deleteCookies("remember-me")
+//                .deleteCookies("remember-me")
+        .and()
+                .rememberMe()
+                    .rememberMeParameter("remember") // default 값은 remember-me
+                    .tokenValiditySeconds(3600)
+                    .userDetailsService(userDetailsService)
                 ;
+
     }
 
 }
